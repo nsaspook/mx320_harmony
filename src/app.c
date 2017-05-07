863122366e3dac293f329156006ebf1cc60f4c9a
root <root@impclean.mchp-main.com>
2017-05-07 16:23:09 -0700

    updates

---
+++ src/app.c
@@ -0,0 +1,210 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+  
+  Company:
+    Microchip Technology Inc.
+  
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It 
+    implements the logic of the application's state machine and it may call 
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
+
+Microchip licenses to you the right to use, modify, copy and distribute
+Software only when embedded on a Microchip microcontroller or digital signal
+controller that is integrated into your product or third party product
+(pursuant to the sublicense terms in the accompanying license agreement).
+
+You should refer to the license agreement accompanying this Software for
+additional information regarding your rights and obligations.
+
+SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
+MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
+IN NO EVENT SHALL MICROCHIP OR ITS LICENSORS BE LIABLE OR OBLIGATED UNDER
+CONTRACT, NEGLIGENCE, STRICT LIABILITY, CONTRIBUTION, BREACH OF WARRANTY, OR
+OTHER LEGAL EQUITABLE THEORY ANY DIRECT OR INDIRECT DAMAGES OR EXPENSES
+INCLUDING BUT NOT LIMITED TO ANY INCIDENTAL, SPECIAL, INDIRECT, PUNITIVE OR
+CONSEQUENTIAL DAMAGES, LOST PROFITS OR LOST DATA, COST OF PROCUREMENT OF
+SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
+(INCLUDING BUT NOT LIMITED TO ANY DEFENSE THEREOF), OR OTHER SIMILAR COSTS.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files 
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+    
+    Application strings and buffers are be defined outside this structure.
+ */
+
+APP_DATA appData;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+ */
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+ */
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize(void)
+{
+	/* Place the App state machine in its initial state. */
+	appData.state = APP_STATE_INIT;
+
+
+	/* TODO: Initialize your application's state machine and other
+	 * parameters.
+	 */
+}
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Tasks(void)
+{
+	static uint32_t i = 0, j = 0;
+	static uint8_t cylon = 0xff;
+	static int32_t alive_led = 0;
+	static bool LED_UP = true;
+
+	/* Check the application's current state. */
+	switch (appData.state) {
+		/* Application's initial state. */
+	case APP_STATE_INIT:
+	{
+		bool appInitialized = true;
+
+
+		if (appInitialized) {
+
+			appData.state = APP_STATE_SERVICE_TASKS;
+		}
+		break;
+	}
+
+	case APP_STATE_SERVICE_TASKS:
+	{
+		if (i++ > 100000) {
+			i = 0;
+
+			if (j++ >= 1) { // delay a bit ok
+				if (0) { // screen status feedback
+					PORTE = ~cylon; // roll leds cylon style
+				} else {
+					PORTE = cylon; // roll leds cylon style (inverted)
+				}
+
+				if (LED_UP && (alive_led != 0)) {
+					alive_led = alive_led << 1;
+					cylon = cylon << 1;
+				} else {
+					if (alive_led != 0) alive_led = alive_led >> 1;
+					cylon = cylon >> 1;
+				}
+				if (alive_led < 2) {
+					alive_led = 2;
+					LED_UP = true;
+					//                mPORTFToggleBits(BIT_0);
+					//                mPORTGToggleBits(BIT_7 | BIT_8);
+				} else {
+					if (alive_led > 128) {
+						alive_led = 128;
+						LED_UP = false;
+						//                    mPORTFToggleBits(BIT_0);
+						//                    mPORTGToggleBits(BIT_7 | BIT_8);
+					}
+				}
+				j = 0;
+			}
+		}
+		break;
+	}
+
+		/* TODO: implement your application state machine.*/
+
+
+		/* The default state should never be executed. */
+	default:
+	{
+		/* TODO: Handle error in application's state machine. */
+		break;
+	}
+	}
+}
+
+
+
+/*******************************************************************************
+ End of File
+ */
