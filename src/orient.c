8ba4ec9d67cd8a48395108fddc25616d4a5a5a78
Fred Brooks <fred@sma2.rain.com>
2017-08-13 16:38:44 -0700

    updates

--- src/orient.c
+++ src/orient.c
@@ -3,22 +3,23 @@
 #include "bioshield.h"
 
 
-struct orienter_data_t orienter_motor= {
-	.old_orienter_bits=0xff,
+struct orienter_data_t orienter_motor = {
+	.old_orienter_bits = 0xff,
 };
 
-struct orienter_bits_t * orienter_bits=(void *)&orienter_motor.orienter_bits;
+struct orienter_bits_t * orienter_bits = (void *) &orienter_motor.orienter_bits;
 
 int32_t orienter_motor_check(int param1, int param2)
 {
 	char headder[16];
 	static uint32_t display_delay = 0;
 
-	orienter_motor.orienter_bits=0;
+	orienter_motor.orienter_bits = 0;
 	orienter_motor.orienter_bits = pbsw1 + (pbsw2 << 1) + (pbsw3 << 2) + (pbsw4 << 3);
 	LEDBAR = orienter_motor.orienter_bits;
 
 	if ((orienter_motor.orienter_bits != orienter_motor.old_orienter_bits) || (display_delay++ == 500000)) {
+		LD8 = !LD8;
 		display_delay = 0;
 		orienter_motor.old_orienter_bits = orienter_motor.orienter_bits;
 		OledClearBuffer();
